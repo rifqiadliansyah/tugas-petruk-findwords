74a6d245b5156356f3722a05199feb0e6d102c8c
f4moUSe <mhanifp27@gmail.com>
2021-05-02 11:33:12 +0700

    Bug Fix
    Membetulkan Bug Reverse dan lain sebagainya

--- tugas_petruk_findworks_2_.cpp
+++ tugas_petruk_findworks_2_.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 const int cols = 16, rows = 15;
 
- char words[rows][cols] = {"tgbwwinterwsesn",
+ char words[rows][cols] =      {"tgbwwinterwsesn",
                                 "aaunttmmhfoodnb",
                                 "jlwcqldzmpmvdmr",
                                 "asagmquwvvbsohi",
@@ -20,44 +20,73 @@ const int cols = 16, rows = 15;
                                 "pdcrzmsngrdnrpz",
                                 "ohnkzwaterjgtra"};
 
-// char *getWordVertical(int)
-// char *reverse(char *);
-// bool searchVertical(char *);
-bool searchHorizontal(char *word){
-        bool buktikan;
-        char *counter;
-        for(int i=0;i<rows;i++){
-            counter= *(words+i);
-            cout<<counter;                      //masih terdapat bug
-            if(strstr(counter, word))
+char *getWordVertical(int a){
+	 static char counter[16] ;
+	 for(int i=0;i<rows;i++){
+            counter[i]= words[i][a];   
+    }
+	return counter;	
+}
+
+char *reverse(char *word){
+
+	static char simpan[16];
+    for(int i=0;i<(int)strlen(word);i++)
+{
+    simpan[i]=word[14-i];
+    
+}
+
+return simpan;
+}
+
+ bool searchVertical(char *word){
+ 	bool buktikan;
+ 	char *counter;
+ 	for (int a = 0 ; a < 15; a++){
+ 		counter  = getWordVertical(a);
+ 		if(strstr(counter, word) || strstr(reverse(counter), word))
                {
-                 buktikan = true;
-                 break;
+                buktikan = true;
+                break;
                }
-            else
-               {
+               else {
                 buktikan = false;
-               }        
+               }    
+	    
     }
         return buktikan;      
     }
 
-
-int main()
-{    
+bool searchHorizontal(char *word){
+        bool buktikan;
+        char *counter;
+        for(int i = 0; i < rows ; i++){
+            counter = *(words+i);            
+           if(strstr(counter, word) || strstr(reverse(counter), word))
+               {
+                buktikan = true;
+                break;
+               }
+               else {
+                buktikan = false;
+               }    
+	    
+    }
+        return buktikan;      
+    }
+int main(){    
     char word[16];
     int n;
     cin>>n;
-    cin.ignore(n);
+    cin.ignore(n,'\n');
     for (int i=0;i<n;i++){
         cin.getline(word,16,'\n');
-        if (/*searchVertical(word) ||*/ searchHorizontal(word)==true)
+        if (searchVertical(word) || searchHorizontal(word))
             cout << "Ada\n";
         else 
-            cout << "Tidak Ada\n";
+          cout << "Tidak Ada\n";
             
     }
     return 0;
-    
-    
 }
